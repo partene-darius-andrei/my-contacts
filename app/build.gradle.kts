4f94ad082f17f6bf2e02e892f4ae096f6e008900
Darius Partene <partene.darius@gmail.com>
2024-02-26 23:40:51 +0200

    showing avatar + styling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,4 +91,5 @@ dependencies {
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
     implementation ("com.google.code.gson:gson:2.10.1")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/my/contacts/ui/list/ContactsListAdapter.kt
+++ app/src/main/java/com/my/contacts/ui/list/ContactsListAdapter.kt
@@ -3,12 +3,14 @@ package com.my.contacts.ui.list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.my.contacts.R
 import com.my.contacts.databinding.FragmentContactsListItemBinding
 import com.my.contacts.models.Contact
 import javax.inject.Inject
 
-class ContactsListAdapter @Inject constructor()
-    : RecyclerView.Adapter<ContactsListAdapter.ContactViewHolder>() {
+class ContactsListAdapter @Inject constructor() :
+    RecyclerView.Adapter<ContactsListAdapter.ContactViewHolder>() {
 
     private val _contacts = mutableListOf<Contact>()
     var contacts: List<Contact> = _contacts
@@ -29,13 +31,19 @@ class ContactsListAdapter @Inject constructor()
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
         with(holder.binding) {
             contacts[position].let {
+                Glide
+                    .with(root.context)
+                    .load(it.avatar)
+                    .error(R.drawable.ic_error)
+                    .circleCrop()
+                    .into(avatar)
                 firstName.text = it.firstName
                 lastName.text = it.lastName
             }
         }
     }
 
-    inner class ContactViewHolder(val binding: FragmentContactsListItemBinding)
-        :RecyclerView.ViewHolder(binding.root)
+    inner class ContactViewHolder(val binding: FragmentContactsListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
 }
