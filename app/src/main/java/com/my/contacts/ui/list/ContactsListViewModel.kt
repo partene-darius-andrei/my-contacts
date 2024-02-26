3e1cc4d795b8e75b213bdfa70d7f41794720cb53
Darius Partene <partene.darius@gmail.com>
2024-02-26 23:05:00 +0200

    added network check

--- app/src/main/java/com/my/contacts/ui/list/ContactsListViewModel.kt
+++ app/src/main/java/com/my/contacts/ui/list/ContactsListViewModel.kt
@@ -1,17 +1,23 @@
 package com.my.contacts.ui.list
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.my.contacts.models.Contact
 import com.my.contacts.repositories.ContactsRepository
+import com.my.contacts.utils.isOnline
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
+@SuppressLint("StaticFieldLeak")
 class ContactsListViewModel @Inject constructor(
-    private val contactsRepository: ContactsRepository
+    private val contactsRepository: ContactsRepository,
+    @ApplicationContext private val context: Context
 ) : ViewModel() {
 
     private val _state: MutableLiveData<ViewState> = MutableLiveData(ViewState.Loading)
@@ -19,11 +25,15 @@ class ContactsListViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            val result = contactsRepository.getContacts()
-            if (result.isSuccessful && result.body()?.data != null) {
-                _state.value = ViewState.Data(result.body()!!.data)
+            if (isOnline(context).not()) {
+                _state.value = ViewState.NoInternetConnection
             } else {
-                _state.value = ViewState.Error
+                val result = contactsRepository.getContacts()
+                if (result.isSuccessful && result.body()?.data != null) {
+                    _state.value = ViewState.Data(result.body()!!.data)
+                } else {
+                    _state.value = ViewState.Error
+                }
             }
         }
     }
---
+++ app/src/main/java/com/my/contacts/utils/NetworkExtensions.kt
@@ -0,0 +1,20 @@
+package com.my.contacts.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+fun isOnline(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val capabilities =
+        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+    return capabilities?.let {
+        when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    } ?: false
+}
